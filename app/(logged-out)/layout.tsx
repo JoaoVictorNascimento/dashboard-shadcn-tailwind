e4bc630bdcf6a86aaa01193a8429e619ef245af2
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2025-06-14 18:40:38 -0300

    feat: create change light and dark button

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,9 +1,16 @@
+import { LightDarkToogle } from "@/components/ui/light-dark-toogle";
+
 export default function LoggedOutLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <div className="flex flex-col gap-4 min-h-screen p-24  items-center justify-center">
-        {children}
-    </div>;
+    return (
+        <>
+            <div className="flex flex-col gap-4 min-h-screen p-24  items-center justify-center">
+                {children}
+            </div>
+            <LightDarkToogle className="fixed right-0 top-1/2" />
+        </>
+    )
 }
