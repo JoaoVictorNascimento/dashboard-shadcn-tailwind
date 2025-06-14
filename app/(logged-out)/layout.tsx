c6cfbc1db1227a26dcf12739f45de4eac13101e8
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2025-06-14 18:19:55 -0300

    feat: initial project, create initial landing page, dark mode provide

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,9 @@
+export default function LoggedOutLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <div className="flex flex-col gap-4 min-h-screen p-24  items-center justify-center">
+        {children}
+    </div>;
+}
